4eebe61e86760405a531e36fbf3cf311d3cd29c2
Cezary <cskura@mobiquityinc.com>
2021-02-10 08:18:32 +0100

    Added DSL

---
+++ DSL/nodejsdocker.groovy
@@ -0,0 +1,27 @@
+
+job('NodeJS Docker example') {
+    scm {
+        git('git://github.com/skuracj/simple-nodejs-app.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL User')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('cskura / my_public_repo')
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+}
